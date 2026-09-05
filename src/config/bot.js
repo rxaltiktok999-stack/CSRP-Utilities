e61c3dbc9408a7c22a9d88912038f899762d6cfc
rxaltiktok999-stack <rxaltiktok999@gmail.com>
2026-09-05 15:31:28 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,7 +23,7 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "", // required by Discord API, not shown in the client
+        name: "CSRP OT", // required by Discord API, not shown in the client
         state: "CSRP OT",     // this is what people actually see
         type: 4,               // Custom
       },
