9f879a59d89405d99687b8e4cf0399a1024a6d51
rxaltiktok999-stack <rxaltiktok999@gmail.com>
2026-09-05 15:22:47 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,8 +23,8 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Custom Status", // required by Discord API, not shown in the client
-        state: "stalking",     // this is what people actually see
+        name: "", // required by Discord API, not shown in the client
+        state: "",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
